faf301af4d2269d5a89c644fcc7b5351eadb5a44
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-11-21 21:52:36 +0300

    Task 1_1

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b * b)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
