acd0e0f83fb8a9c0d437d5485b4497e1068705cf
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-11-25 23:53:23 +0300

    Task 2

---
+++ HOMEWORK_1/2/Program.cs
@@ -0,0 +1,10 @@
+﻿int Num(int n)
+{
+    Console.WriteLine($"Our number: {n}");
+    int ost = n % 10;
+    n = n / 100;
+    int num = n * 10 + ost;
+    return num;
+}
+
+Console.WriteLine(Num(new Random().Next(100,1000)));
