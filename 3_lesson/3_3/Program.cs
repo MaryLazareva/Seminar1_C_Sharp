ee06932cc50efae2a1f132e3f0b757d0d91fbaac
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-11-28 21:19:50 +0300

    Task 3_3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,25 @@
+﻿void Tabl(int n)
+{
+    int count = 1;
+    while (count <= n)
+        {
+            if (count == n)
+            {
+                double kvadrat = Math.Pow(count,2);
+                
+                Console.Write($"{kvadrat}");
+            }
+            else
+            {
+                double kvadrat = Math.Pow(count,2);
+                
+                Console.Write($"{kvadrat}, ");
+            }
+            
+            count +=1;
+        }
+
+
+}
+Console.WriteLine("Введите число:");
+Tabl(int.Parse(Console.ReadLine()));
