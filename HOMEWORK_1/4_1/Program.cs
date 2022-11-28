5d1a09dec3566f6cfb1bec0dd21f0c903728a023
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-11-28 20:24:07 +0300

    Task 4_1

---
+++ HOMEWORK_1/4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿string Number(int n)
+{
+    if (n > 99)
+    {   
+        while (n > 1000)
+        {
+            n = n / 10;
+        }   
+
+        return $"Number is {n % 10}";
+    }   
+   
+    else  
+        return "not have third number";
+}
+
+Console.WriteLine("Input number:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Number(num));
