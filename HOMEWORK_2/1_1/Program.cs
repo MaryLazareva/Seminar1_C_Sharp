9eea88f0af05188a101250e272717449ec705ac9
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-11-29 11:54:13 +0300

    Task 1_1

---
+++ HOMEWORK_2/1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿string Polindrom(int n)
+{ 
+    int num = n / 10000;
+    int num1 = n % 10;
+    if (num == num1)
+    {
+        n = (n - num * 10000)/10;
+        num = n / 100;
+        num1 = n % 10;
+        if (num == num1)
+            return "Полиндром";
+        else
+            return "Не полиндром";
+    }   
+    else
+        return "Не полиндром";    
+}
+
+Console.WriteLine("Введите число:");
+Console.WriteLine(Polindrom(int.Parse(Console.ReadLine())));
