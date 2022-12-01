554806557495090881aae1adaa04dd60cb529e93
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-01 20:37:42 +0300

    Task 0

---
+++ 4_lesson/4_0/Program.cs
@@ -0,0 +1,12 @@
+﻿int Sum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <=n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число:");
+Console.WriteLine(Sum(int.Parse(Console.ReadLine())));
