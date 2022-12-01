3e78d5d5b7e264e5ce21c3fdccc643e82a3d587e
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-01 20:44:28 +0300

    Task 1

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,9 @@
+﻿int Num(int n)
+{
+    string num = n.ToString();
+    int dlina = num.Length;
+    return dlina;
+}
+
+Console.WriteLine("Введите число:");
+Console.WriteLine(Num(int.Parse(Console.ReadLine())));
