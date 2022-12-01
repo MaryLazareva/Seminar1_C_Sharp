47ff15f968330cb04c1ff9559f4755d3cc3cca69
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-01 20:49:18 +0300

    Task 2

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int Proiz(int n)
+{
+    int pr = 1;
+    for (int i = 1; i <=n; i++)
+    {
+        pr *= i;
+    }
+    return pr;
+}
+
+Console.WriteLine("Введите число:");
+Console.WriteLine(Proiz(int.Parse(Console.ReadLine())));
