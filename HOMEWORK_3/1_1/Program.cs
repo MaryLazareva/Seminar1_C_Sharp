af8574d1aaad1a3e5d31d34170384e4ba90920c7
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-02 14:41:22 +0300

    Task 1_1

---
+++ HOMEWORK_3/1_1/Program.cs
@@ -0,0 +1,35 @@
+﻿int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+void Stepen(double num1, double num2)
+{
+    double pr = 1;
+    
+    if (num2 >= 0)
+    {
+        for (int i = 0; i < num2; i++)
+        {
+            pr *= num1;
+        }
+        Console.WriteLine($"{num1}^{num2} = {pr}");
+    }
+    else
+    {
+        for (int i = 0; i < -(num2); i++)
+        {
+            pr *= 1/(num1);
+           
+        }
+        Console.WriteLine($"{num1}^{num2} = {pr}");
+    }   
+       
+}
+
+int a = ReadNumber("Введите число A:");
+int b = ReadNumber("Введите число B:");
+Stepen(a,b);
