0480fec00cfab4d5854596bd1ad4f7e69b375e43
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-08 21:00:43 +0300

    Task 6_2

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,13 @@
+﻿string Bin(int n)
+{
+    string result = "";
+
+    while (n > 0)
+    {
+        result = n % 2 + result;
+        n /= 2;
+    }
+    return result;
+}
+
+Console.WriteLine(Bin(int.Parse(Console.ReadLine())));
