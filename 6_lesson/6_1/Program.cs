b0f7303c49b766b161e989f7891c42f21f995cb7
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-08 20:45:42 +0300

    Task 6_1

---
+++ 6_lesson/6_1/Program.cs
@@ -0,0 +1,13 @@
+﻿void TryG()
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    int c = int.Parse(Console.ReadLine());
+
+    if (a < b + c & b < a + c & c < a + b)
+        Console.WriteLine("YES");
+    else
+        Console.WriteLine("NO");
+}
+
+TryG();
