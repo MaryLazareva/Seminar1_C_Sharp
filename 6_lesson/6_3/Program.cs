b095825d282e78f0b8ac80526a788fd0448d7bea
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-08 21:08:41 +0300

    Task 6_3

--- 6_lesson/6_3/Program.cs
+++ 6_lesson/6_3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿void Fibbonacci(int num)
+{
+    int a = 0, b = 1;
+
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"{a} ");
+        (a, b) = (b, a + b);
+    }
+
+}
+
+Fibbonacci(int.Parse(Console.ReadLine()));
