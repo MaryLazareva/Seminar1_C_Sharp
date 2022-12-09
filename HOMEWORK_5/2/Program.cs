2870f26745adac0deed30825ad972a864af2561a
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-09 23:32:12 +0300

    Task 2

---
+++ HOMEWORK_5/2/Program.cs
@@ -0,0 +1,15 @@
+﻿void Points(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1)/(k1 - k2);
+
+    double y = k1 * (b2 - b1)/(k1 - k2) + b1;
+
+    Console.WriteLine($"({x};{y})");
+}
+
+Points(double.Parse(Console.ReadLine()),
+       double.Parse(Console.ReadLine()),
+       double.Parse(Console.ReadLine()),
+       double.Parse(Console.ReadLine()));
+
+
