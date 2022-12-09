97da1fa529c33c5ab536f18304c6e76d1500ecce
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-09 23:12:35 +0300

    Task 1

---
+++ HOMEWORK_5/1/Program.cs
@@ -0,0 +1,18 @@
+﻿void Number()
+{
+    bool result = true;
+    int count = 0;
+
+    while (result)
+    {
+        string num = Console.ReadLine();
+
+        if (num == "")
+            result = false;
+        else if (int.Parse(num) > 0)
+            count++;
+    }
+    Console.WriteLine($"Количество положительных чисел равно {count}");
+}
+Number();
+
