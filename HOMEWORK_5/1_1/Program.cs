6b0350cf6ddb2d4f675a0b8d83c4765b1bacc079
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-12 21:43:04 +0300

    All tasks for 7 lesson

---
+++ HOMEWORK_5/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿void Num(string n)
+{
+    int count = 0;
+    while (n != "")
+    {
+      if (int.Parse(n) > 0)
+        count++; 
+
+      n = Console.ReadLine();
+    }
+    Console.WriteLine(count);
+}
+Num(Console.ReadLine());
