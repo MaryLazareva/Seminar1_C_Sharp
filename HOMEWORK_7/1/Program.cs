0a419639833bc75268ac61be156ae33f97e33b71
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-18 22:45:16 +0300

    Task 1

---
+++ HOMEWORK_7/1/Program.cs
@@ -0,0 +1,52 @@
+﻿void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+int[,] SelectionSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int maxPosition = j;
+        
+            for (int k = j + 1; k < array.GetLength(1); k++)
+                if (array[i, maxPosition] < array[i,k]) maxPosition = k;
+            
+            (array[i,j],array[i, maxPosition]) = (array[i, maxPosition],array[i,j]);
+        }
+    }   
+    return array;
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter range of numbers:");
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+int[,] arr_sort = SelectionSort(arr_1);
+Print(arr_sort);
