968ea04e8e7f3eaa170ea84743df2296bd04014a
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2022-12-20 00:04:04 +0300

    Task 1 HW

---
+++ HOMEWORK_8/1/Program.cs
@@ -0,0 +1,18 @@
+﻿void Numbers(int m, int n)
+{
+    if (m > n) return;
+
+    if (m % 2 == 0) 
+    {   
+        Console.Write($" {m} ");
+        Numbers(m+2, n);
+    }
+    else
+    {
+        m += 1;
+        Console.Write($" {m} ");
+        Numbers(m+2, n);
+    }           
+}
+Console.WriteLine("Enter Numbers: ");
+Numbers(int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine()));
